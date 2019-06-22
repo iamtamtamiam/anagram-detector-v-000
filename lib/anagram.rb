658fbcfa22c722d55808d0ae7ef1a8ed26ac4974
Tam Vu <vutam_a@hotmail.com>
2019-06-22 19:01:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,9 @@ class Anagram
   end 
   
   def match(some_words)
-    some_words.select 
+    some_words.select |anagram|
+      anagram.split("").sort == @word.split("").sort
+    end 
     
     #split each word in some_words then go through each word and match it to @word.split
     
