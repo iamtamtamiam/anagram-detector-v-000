cda8152a8c219c94b17fac4bf30fa0ab722d3247
Tam Vu <vutam_a@hotmail.com>
2019-06-22 18:59:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   end 
   
   def match(some_words)
-    some_words.
+    some_words.select 
     
     #split each word in some_words then go through each word and match it to @word.split
     
